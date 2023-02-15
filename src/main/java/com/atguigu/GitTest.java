9809cf8a8a8473912b19aa8f0ef45a32dcfc8e50
hcpfighting <125455285+hcpfighting@users.noreply.github.com>
2023-02-16 00:00:04 +0800

    Update GitTest.java
    pull commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("4 master");
         System.out.println("4 host");
         System.out.println("本地该1");
+        System.out.println("远程改1");
     }
 }
